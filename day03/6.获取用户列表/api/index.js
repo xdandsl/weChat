47c8f0036095633a562e78ba3f2b52f75041be5d
xdandsl <15733221796@163.com>
2018-11-19 10:42:42 +0800

    day3之第一，新增api文件夹，封装所有接口模块，便于后期维护。第二，新增了创建标签方法createMenu和删除标签方法deleteMenu在weChat文件夹中

---
+++ day03/6.获取用户列表/api/index.js
@@ -0,0 +1,18 @@
+/**
+ * 接口管理模块（把所有的url接口单独放到一个模块中，防止后期接口发生变动，便于后期的维护）
+ */
+
+const profix = `https://api.weixin.qq.com/`;
+//原则：把所有接口中，不变的量实时获取，变得量放在此模块中。。。
+module.exports = {
+  accessToken:`${profix}cgi-bin/token?`,
+  menu:{
+    createMenu:`${profix}cgi-bin/menu/create?`,
+    deleteMenu:`${profix}cgi-bin/menu/delete?`
+  },
+  tag:{
+    createTag:`${profix}cgi-bin/tags/create?`,
+    deleteTag:`${profix}cgi-bin/tags/delete?`
+  }
+};
+
